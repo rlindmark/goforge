bf3c0d0be5a7d66829516d97a366540958086dce
Roger <roger@example.com>
2023-11-29 23:21:38 +0100

    Added initial owner with tests

---
+++ owner.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"regexp"
+)
+
+type Owner struct {
+	Uri         string `json:"uri"`
+	Slug        string `json:"slug"`
+	Username    string `json:"username"`
+	Gravatar_id string `json:"gravatar_id"`
+}
+
+func isValidOwnerSlug(slug string) (bool, error) {
+	return regexp.MatchString("^[a-zA-Z0-9]+$", slug)
+}
+
+func NewOwner(uri string, slug string, username string, gravatar_id string) (*Owner, error) {
+	ok, _ := isValidOwnerSlug(slug)
+	if !ok {
+		return nil, fmt.Errorf("Cant create Owner with illegal slug: %v", slug)
+	}
+	return &Owner{uri, slug, username, gravatar_id}, nil
+}
+
+func (owner *Owner) asJson() string {
+	json := fmt.Sprintf("{")
+	json += fmt.Sprintf("%q:%q,", "uri", owner.Uri)
+	json += fmt.Sprintf("%q:%q,", "slug", owner.Slug)
+	json += fmt.Sprintf("%q:%q,", "username", owner.Username)
+	json += fmt.Sprintf("%q:%q", "gravatar_id", owner.Gravatar_id)
+	json += fmt.Sprintf("}")
+	return json
+}
---
+++ owner_test.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestIsValidOwnerSlug(t *testing.T) {
+
+	var testCases = []struct {
+		slug   string
+		expect bool
+	}{
+		{"puppetlabs", true},
+		{"Puppetlabs", true},
+		{"p", true},
+		{"0", true},
+		{"puppet labs", false},
+		{"", false},
+	}
+
+	for _, test := range testCases {
+
+		result, _ := isValidOwnerSlug(test.slug)
+
+		if result != test.expect {
+			fmt.Printf("For %v: got %v, expected %v", test.slug, result, test.expect)
+		}
+	}
+}
+
+func TestNewOwner(t *testing.T) {
+
+	var testCases = []struct {
+		uri         string
+		slug        string
+		username    string
+		gravatar_id string
+		expect      bool
+	}{
+		{"puppetlabs", "puppetlabs", "puppetlabs", "gravatar", true},
+		{"puppetlabs", "", "puppetlabs", "gravatar", false},
+		{"puppetlabs", "puppet labs", "puppetlabs", "gravatar", false},
+	}
+
+	for _, test := range testCases {
+
+		result, err := NewOwner(test.uri, test.slug, test.username, test.gravatar_id)
+
+		if result == nil && test.expect != false {
+			fmt.Printf("For %v: got %v err=%v, expected %v", test.slug, result, err, test.expect)
+		}
+		if result != nil && test.expect != true {
+			fmt.Printf("For %v: got %v err=%v, expected %v", test.slug, result, err, test.expect)
+		}
+	}
+}
+
+func TestOwnerAsJson(t *testing.T) {
+
+	var testCases = []struct {
+		uri         string
+		slug        string
+		username    string
+		gravatar_id string
+		expect      string
+	}{
+		{"Puppetlabs", "Slug", "Username", "Gravatar",
+			"{\"uri\":\"Puppetlabs\",\"slug\":\"Slug\",\"username\":\"Username\",\"gravatar_id\":\"Gravatar\"}",
+		},
+	}
+
+	for _, test := range testCases {
+
+		user, _ := NewOwner(test.uri, test.slug, test.username, test.gravatar_id)
+
+		result := user.asJson()
+		if result != test.expect {
+			fmt.Printf("Got %v, expected %v", result, test.expect)
+		}
+	}
+}
